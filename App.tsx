a693139710708c3dfad8099ea1bab3d93660ca77
Jéssica Neri <jessica.neri@cubos.io>
2022-09-18 18:03:15 -0300

    feat: mobile app finished

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
+import { useRef, useEffect } from "react";
 import { Background } from "./src/components/Background";
 import { StatusBar } from "react-native";
-import { Home } from "./src/screens/Home";
+import { Routes } from "./src/routes";
+import * as Notifications from "expo-notifications";
 
 import {
   useFonts,
@@ -10,6 +12,11 @@ import {
   Inter_900Black,
 } from "@expo-google-fonts/inter";
 import { Loading } from "./src/components/Loading";
+import { Subscription } from "expo-modules-core";
+
+import "./src/services/notificationConfigs";
+
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +25,40 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(notification);
+      });
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
+
   return (
     <Background>
       <StatusBar
@@ -25,7 +66,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
---
+++ src/components/DuoMatch/index.tsx
@@ -0,0 +1,68 @@
+import {
+  View,
+  Modal,
+  ModalProps,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { CheckCircle } from "phosphor-react-native";
+import * as Clipboard from "expo-clipboard";
+
+import { styles } from "./styles";
+import { THEME } from "../../theme";
+import { Heading } from "../Heading";
+import { useState } from "react";
+
+interface Props extends ModalProps {
+  discord: string;
+  onClose: () => void;
+}
+
+export function DuoMatch({ discord, onClose, ...props }: Props) {
+  const [isCopy, setIsCopy] = useState(false);
+  async function handleCopyDiscordToClipboard() {
+    setIsCopy(true);
+    await Clipboard.setStringAsync(discord);
+
+    Alert.alert("Discord copiado!", "Usuário copiado para uso no Discord.");
+    setIsCopy(false);
+  }
+  return (
+    <Modal {...props} transparent statusBarTranslucent animationType="fade">
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <TouchableOpacity style={styles.closeIcon} onPress={onClose}>
+            <MaterialIcons
+              name="close"
+              size={20}
+              color={THEME.COLORS.CAPTION_500}
+            />
+          </TouchableOpacity>
+          <CheckCircle size={64} color={THEME.COLORS.SUCCESS} weight="bold" />
+          <Heading
+            title="Let's play!"
+            subtitle="Agora é só começar a jogar!"
+            style={{ alignItems: "center", marginTop: 24 }}
+          />
+          <Text style={styles.label}>Adicione no Discord</Text>
+          <TouchableOpacity
+            style={styles.discordButton}
+            onPress={handleCopyDiscordToClipboard}
+            disabled={isCopy}
+          >
+            <Text style={styles.discord}>
+              {isCopy ? (
+                <ActivityIndicator color={THEME.COLORS.PRIMARY} />
+              ) : (
+                discord
+              )}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+}
